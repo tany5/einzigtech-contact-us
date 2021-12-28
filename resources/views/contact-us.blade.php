22c556535ad52d390e20f02621582ff23c418b2a
unknown <tanmay1dey@gmail.com>
2021-12-28 20:02:05 +0530

    job_that_has_been_done

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -6,9 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
+</head>
 
 <body>
 </body>
-</head>
-
 </html>
